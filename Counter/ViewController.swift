a971204babfaeebd53766bfe3c1f77b5a0edcf82
Beton <dima41768@icloud.com>
2025-07-13 16:57:22 +0300

    Merge pull request #1 from DmitryBeton/sprint_03
    Итоговая версия приложения

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,29 +9,38 @@ import UIKit
 
 final class ViewController: UIViewController {
 
-    private var count: Int = 0
-    
+    // MARK: - Outlets
     @IBOutlet private weak var labelCountDisplay: UILabel!
-    
     @IBOutlet private weak var buttonSubstract: UIButton!
-    
     @IBOutlet private weak var buttonAdd: UIButton!
-    
     @IBOutlet private weak var buttonClear: UIButton!
-    
     @IBOutlet private weak var textViewHistory: UITextView!
     
+    // MARK: - Properties
+    private var count: Int = 0
+    private var history: [String] = []
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        count = UserDefaults.standard.integer(forKey: "count")
+        history = UserDefaults.standard.stringArray(forKey: "history") ?? []
         textViewHistory.text += "История изменений:"
+        // Чтение массива
+        for line in history {
+            historyPrint(line)
+        }
+        // обновление счетчика
+        updateCountDisplay()
     }
 
+    // MARK: - Private Methods
     // обновление счетчика
     private func updateCountDisplay() {
         labelCountDisplay.text = "Значение счётчика: \(count)"
     }
-    
     // получение нынешней даты и время
     private func timeNow() -> String {
         let getDate = Date()
@@ -39,13 +48,18 @@ final class ViewController: UIViewController {
         dateFormatter.dateFormat = "dd.MM.yyyy, HH:mm:ss"
         return dateFormatter.string(from: getDate)
     }
-    
     // метод для вывода текста в UITextView
     private func historyPrint(_ s: String) {
         textViewHistory.text += "\n\(s)"
         scrollTextViewToBottom(textViewHistory)
     }
-    
+    // метод для сохранения истории изменений
+    private func historySave(_ s: String) {
+        history += [s]
+        UserDefaults.standard.set(history, forKey: "history")
+//        print("Данные сохранены: \(UserDefaults.standard.stringArray(forKey: "history") ?? ["error"])")
+        historyPrint(s)
+    }
     // scroll вниз за новым текстом
     private func scrollTextViewToBottom(_ textView: UITextView) {
         if textView.text.count > 0 {
@@ -55,29 +69,28 @@ final class ViewController: UIViewController {
         }
     }
     
+    // MARK: - Actions
     @IBAction private func subtractOne() {
         count -= 1
         if count < 0 {
             count = 0
-            historyPrint("\(timeNow()): попытка уменьшить значение счётчика ниже 0")
+            historySave("\(timeNow()): попытка уменьшить значение счётчика ниже 0")
         } else {
-            historyPrint("\(timeNow()): значение изменено на -1")
+            historySave("\(timeNow()): значение изменено на -1")
         }
+        UserDefaults.standard.set(count, forKey: "count")
         updateCountDisplay()
     }
-    
     @IBAction private func addOne() {
         count += 1
-        historyPrint("\(timeNow()): значение изменено на +1")
+        historySave("\(timeNow()): значение изменено на +1")
+        UserDefaults.standard.set(count, forKey: "count")
         updateCountDisplay()
     }
-        
     @IBAction private func resetCount() {
         count = 0
-        historyPrint("\(timeNow()): значение сброшено")
-
+        historySave("\(timeNow()): значение сброшено")
+        UserDefaults.standard.set(count, forKey: "count")
         updateCountDisplay()
     }
-
 }
-
